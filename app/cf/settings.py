8f261d24d1343637b21ee473a59c40262222a25a
swelanauguste <swelanauguste@gmail.com>
2024-08-16 19:52:46 -0400

    adding pwa

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "after_response",
+    "pwa",
     "users",
     "promotions",
     "page",
@@ -190,11 +191,11 @@ PWA_APP_SCOPE = "/"
 PWA_APP_ORIENTATION = "any"
 PWA_APP_START_URL = "/"
 PWA_APP_STATUS_BAR_COLOR = "default"
-PWA_APP_ICONS = [{"src": "/static/images/160x160.png", "sizes": "160x160"}]
-PWA_APP_ICONS_APPLE = [{"src": "/static/images/160x160.png", "sizes": "160x160"}]
+PWA_APP_ICONS = [{"src": "/static/images/icon1.png", "sizes": "144x144"}]
+PWA_APP_ICONS_APPLE = [{"src": "/static/images/icon1.png", "sizes": "144x144"}]
 PWA_APP_SPLASH_SCREEN = [
     {
-        "src": "/static/images/160x160.png",
+        "src": "/static/images/icon1.png",
         "media": "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
     }
 ]
