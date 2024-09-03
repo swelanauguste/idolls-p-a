4f7ffeec8b847b3a1da3c3faae31a653f92276e7
swelanauguste <swelanauguste@gmai.com>
2024-09-03 21:54:17 +0000

    entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -16,7 +16,7 @@ python manage.py migrate
 python manage.py createsuperuser --noinput
 python manage.py collectstatic --noinput
 # python manage.py add_banners
-python manage.py add_services ./static/docs/services.csv
+# python manage.py add_services ./static/docs/services.csv
 # python manage.py add_socials ./static/docs/socials.csv
 # python manage.py add_values ./static/docs/values.csv
 # python manage.py add_why_us ./static/docs/why_us.csv
